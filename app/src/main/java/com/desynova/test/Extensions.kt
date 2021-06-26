6387fc53e5beaea8475fda68ef609b14845275e0
mirza <mirzaahmed65@gmail.com>
2021-06-26 18:27:06 +0530

    added more control with better UI

---
+++ app/src/main/java/com/desynova/test/Extensions.kt
@@ -0,0 +1,7 @@
+package com.desynova.test
+
+import android.content.res.Resources
+
+fun Int.dpToPx(): Int {
+    return ((this * Resources.getSystem().displayMetrics.density).toInt())
+}
--- app/src/main/java/com/desynova/test/MainActivity.kt
+++ app/src/main/java/com/desynova/test/MainActivity.kt
@@ -4,16 +4,20 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.WindowManager
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.PopupWindow
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.desynova.test.databinding.ActivityMainBinding
-import com.google.android.exoplayer2.C
+import com.github.vkay94.dtpv.youtube.YouTubeOverlay
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.Player.STATE_BUFFERING
+import com.google.android.exoplayer2.Player.STATE_ENDED
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector.SelectionOverride
-import com.google.android.exoplayer2.trackselection.MappingTrackSelector.MappedTrackInfo
-import com.google.android.exoplayer2.util.Assertions
 import com.google.android.exoplayer2.util.Util
 
 
@@ -25,24 +29,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private var exoPlayer: SimpleExoPlayer? = null
-    private var currentLang = 0
-
     private var allLanguages: ArrayList<Pair<String, String>>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        binding.bvLanguage.setOnClickListener {
-            trackSelector?.let {
-                it.setParameters(
-                    it.parameters.buildUpon()
-                        .setPreferredAudioLanguage(allLanguages!![currentLang].second)
-                )
-                Log.d(TAG, "New lang is ${allLanguages!![currentLang].second}")
-                currentLang = (currentLang + 1) % 2
-            }
-
-        }
+        setListeners()
     }
 
     override fun onStart() {
@@ -84,45 +76,94 @@ class MainActivity : AppCompatActivity() {
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
     }
 
-    private fun initializePlayer() {
-        exoPlayer = SimpleExoPlayer.Builder(this).build()
-        binding.exoPlayerView.player = exoPlayer
-        binding.exoPlayerView.useController = true
-        exoPlayer?.playWhenReady = true
+    private fun setListeners() {
+        binding.bvTrack.setOnClickListener {
+            langPopupWindow.showAsDropDown(it)
+            return@setOnClickListener
+        }
 
-        val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp3))
-        exoPlayer?.setMediaItem(mediaItem)
-
-        exoPlayer?.playWhenReady = playWhenReady
-        exoPlayer?.seekTo(currentWindow, playbackPosition)
-        exoPlayer?.prepare()
-
-
-        exoPlayer?.addListener(object : Player.Listener {
-            override fun onPlaybackStateChanged(state: Int) {
-                super.onPlaybackStateChanged(state)
-                if (state == Player.STATE_READY) {
-                    allLanguages = ArrayList()
-                    for (i in 0 until exoPlayer!!.currentTrackGroups.length) {
-                        val format =
-                            exoPlayer?.currentTrackGroups?.get(i)?.getFormat(0)?.sampleMimeType
-                        val lang = exoPlayer?.currentTrackGroups?.get(i)?.getFormat(0)?.language
-                        val id = exoPlayer?.currentTrackGroups?.get(i)?.getFormat(0)?.id
-                        Log.d(TAG, "${exoPlayer?.currentTrackGroups?.get(i)?.getFormat(0)}")
-                        if (format!!.contains("audio") && id != null && lang != null) {
-                            Log.d(TAG, "$lang $id")
-                            allLanguages?.add(Pair(id, lang))
-                        }
-                    }
-                    trackSelector = exoPlayer?.trackSelector as? DefaultTrackSelector
+        binding.bvPayerState.setOnClickListener {
+            Log.d(TAG, "exoPlayer?.playbackState: ${exoPlayer?.playbackState}")
+            when (exoPlayer?.playbackState) {
+                STATE_ENDED -> {
+                    exoPlayer?.seekTo(0)
+                    exoPlayer?.playWhenReady = true
+                }
+                else -> {
+                    if (exoPlayer?.isPlaying == true)
+                        exoPlayer?.pause()
+                    else
+                        exoPlayer?.play()
                 }
             }
+        }
 
+        binding.youtubeOverlay.performListener(object : YouTubeOverlay.PerformListener {
+            override fun onAnimationStart() {
+                binding.youtubeOverlay.visibility = View.VISIBLE
+            }
+
+            override fun onAnimationEnd() {
+                binding.youtubeOverlay.visibility = View.GONE
+            }
         })
-
-
     }
 
+
+    private fun initializePlayer() {
+        exoPlayer = SimpleExoPlayer.Builder(this).build()
+        with(binding.exoPlayerView) {
+            this.player = exoPlayer
+            this.useController = true
+            this.setControllerVisibilityListener {
+                binding.bvPayerState.visibility = it
+                if (!allLanguages.isNullOrEmpty())
+                    binding.bvTrack.visibility = it
+            }
+
+        }
+
+        with(exoPlayer) {
+            this?.setMediaItem(MediaItem.fromUri(getString(R.string.media_url_mp3)))
+            this?.playWhenReady = playWhenReady
+            this?.seekTo(currentWindow, playbackPosition)
+            this?.prepare()
+            this?.addListener(object : Player.Listener {
+
+                override fun onIsPlayingChanged(isPlaying: Boolean) {
+                    super.onIsPlayingChanged(isPlaying)
+                    Log.d(TAG, "onIsPlayingChanged $isPlaying")
+                    updatePlayerState(isPlaying)
+                }
+
+                override fun onPlaybackStateChanged(state: Int) {
+                    super.onPlaybackStateChanged(state)
+                    Log.d(TAG, "Srtate : $state")
+                    if (state == STATE_ENDED)
+                        updatePlayerState(false)
+                    if (state == Player.STATE_READY) {
+                        allLanguages = ArrayList()
+                        for (i in 0 until exoPlayer!!.currentTrackGroups.length) {
+                            val format =
+                                exoPlayer?.currentTrackGroups?.get(i)?.getFormat(0)?.sampleMimeType
+                            val lang = exoPlayer?.currentTrackGroups?.get(i)?.getFormat(0)?.language
+                            val id = exoPlayer?.currentTrackGroups?.get(i)?.getFormat(0)?.id
+                            if (format!!.contains("audio") && id != null && lang != null) {
+                                allLanguages?.add(Pair(id, lang))
+                            }
+                        }
+                        if (!allLanguages.isNullOrEmpty())
+                            binding.bvTrack.visibility = View.VISIBLE
+                        this@MainActivity.trackSelector =
+                            exoPlayer?.trackSelector as? DefaultTrackSelector
+                    }
+                }
+            })
+        }
+
+        binding.youtubeOverlay.player(exoPlayer!!)
+
+    }
 
     private var playWhenReady = true
     private var currentWindow = 0
@@ -139,29 +180,53 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun updatePlayerState(isPlaying: Boolean) {
+        val image = if (isPlaying) {
+            R.drawable.ic_pause
+        } else {
+            if (exoPlayer?.playbackState == STATE_ENDED)
+                R.drawable.ic_replay
+            else
+                R.drawable.ic_play
+        }
+        if (exoPlayer?.playbackState == STATE_BUFFERING) {
+            binding.pbLoader.visibility = View.VISIBLE
+            binding.bvPayerState.visibility = View.GONE
+        } else {
+            binding.pbLoader.visibility = View.GONE
+            binding.bvPayerState.visibility = View.VISIBLE
+        }
+        binding.bvPayerState.setImageResource(image)
+    }
 
-    fun setAudioTrack(track: Int) {
-        println("setAudioTrack: $track")
-        val mappedTrackInfo: MappedTrackInfo =
-            Assertions.checkNotNull(trackSelector?.currentMappedTrackInfo)
-        val parameters: DefaultTrackSelector.Parameters = trackSelector?.parameters ?: return
-        val builder = parameters.buildUpon()
-        for (rendererIndex in 0 until mappedTrackInfo.rendererCount) {
-            val trackType = mappedTrackInfo.getRendererType(rendererIndex)
-            if (trackType == C.TRACK_TYPE_AUDIO) {
-                builder.clearSelectionOverrides(rendererIndex)
-                    .setRendererDisabled(rendererIndex, false)
-                val groupIndex = track - 1
-                val tracks = intArrayOf(0)
-                val override = SelectionOverride(groupIndex, *tracks)
-                builder.setSelectionOverride(
-                    rendererIndex,
-                    mappedTrackInfo.getTrackGroups(rendererIndex),
-                    override
-                )
+    private val langPopupWindow by lazy {
+        PopupWindow(this).apply {
+            isFocusable = true
+            width = 150.dpToPx()
+            height = WindowManager.LayoutParams.WRAP_CONTENT
+            contentView = ListView(this@MainActivity).apply {
+                adapter = ArrayAdapter(
+                    this@MainActivity,
+                    android.R.layout.simple_dropdown_item_1line,
+                    allLanguages!!.map { it.second })
+
+                this.setOnItemClickListener { parent, view, position, id ->
+                    trackSelector?.let {
+                        it.setParameters(
+                            it.parameters.buildUpon()
+                                .setPreferredAudioLanguage(allLanguages!![position].second)
+                        )
+                        Toast.makeText(
+                            this@MainActivity,
+                            allLanguages!![position].second,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    dismiss()
+                }
             }
         }
-        trackSelector?.setParameters(builder)
-//        curentAudioTrack = track
     }
+
+
 }
